c0477bbee69f3ef54f055af4b9d7a1e1af23aafb
Toludoyin <toludoyinshopein@gmail.com>
2024-04-20 17:45:56 +0100

    in-progress

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Setup
 Create a virtual environment
+
 Install DuckDB and connector dbt-duckdb
+
 Extract data from API into DuckDB
+
 Create dbt project
 
 initialize dbt with dbt init
--- stocks_duckdb_dbt/data/extract_load.py
+++ stocks_duckdb_dbt/data/extract_load.py
@@ -3,7 +3,6 @@ import pandas as pd
 import re
 import duckdb 
 
-
 def extract_api_data():
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=demo'
     api_data = requests.get(url)
@@ -15,9 +14,9 @@ def load_to_duckdb(data):
     stocks_data.columns = [re.sub(r'[^a-zA-Z]', '', col) for col in stocks_data.columns]
 
     # create duckdb connection
-    con = duckdb.connect(database="stocks_db.db")
+    con = duckdb.connect("stocks_db.db", read_only=False)
     con.execute("CREATE TABLE IF NOT EXISTS stocks AS SELECT * FROM 'stocks_data'")
-    con.execute("INSERT INTO stocks SELECT * FROM stocks_data") 
+    con.execute("INSERT INTO stocks_db.main.stocks SELECT * FROM stocks_data") 
     # df = con.sql("""
     # SELECT *
     # FROM 'stocks'
@@ -25,7 +24,6 @@ def load_to_duckdb(data):
     # print(df)
     # print(con.sql('SHOW ALL TABLES'))
     # print(con.execute("DESCRIBE stocks_db").df())
-
 
 data_from_api = extract_api_data()
 load_to_duckdb(data_from_api)
