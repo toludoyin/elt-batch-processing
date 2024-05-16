d38d4303b9aae9ff4754c63709ce1fb981fe4da2
Toludoyin <toludoyinshopein@gmail.com>
2024-05-16 07:32:53 +0100

    check data exist in db

---
+++ dags/test_query.py
@@ -0,0 +1,26 @@
+
+from airflow.decorators import dag, task
+from pendulum import datetime
+from duckdb_provider.hooks.duckdb_hook import DuckDBHook
+
+DUCKDB_CONN_ID = "duckdb_conn"
+DUCKDB_TABLE_NAME = "stocks"
+
+
+@dag(start_date=datetime(2024, 5, 1), schedule=None, 
+     catchup=False, tags=["data-exist-in-db"])
+def test_duckdb():
+    @task
+    def query_duckdb(my_table, conn_id):
+        my_duck_hook = DuckDBHook.get_hook(conn_id)
+        conn = my_duck_hook.get_conn()
+
+        data = conn.execute(f"SELECT * FROM {my_table};").fetchall()
+        print(data)
+
+        return data
+
+    query_duckdb(my_table=DUCKDB_TABLE_NAME, conn_id=DUCKDB_CONN_ID)
+
+
+test_duckdb()
