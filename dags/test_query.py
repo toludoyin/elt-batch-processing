5f32277eca166cce6850e917d43ea6c9ae84dd3c
Toludoyin <toludoyinshopein@gmail.com>
2024-05-17 05:38:03 +0100

    add a test query

--- dags/test_query.py
+++ dags/test_query.py
@@ -6,9 +6,9 @@ from duckdb_provider.hooks.duckdb_hook import DuckDBHook
 DUCKDB_CONN_ID = "duckdb_conn"
 DUCKDB_TABLE_NAME = "stocks"
 
-
 @dag(start_date=datetime(2024, 5, 1), schedule=None, 
      catchup=False, tags=["data-exist-in-db"])
+
 def test_duckdb():
     @task
     def query_duckdb(my_table, conn_id):
@@ -16,8 +16,6 @@ def test_duckdb():
         conn = my_duck_hook.get_conn()
 
         data = conn.execute(f"SELECT * FROM {my_table};").fetchall()
-        print(data)
-
         return data
 
     query_duckdb(my_table=DUCKDB_TABLE_NAME, conn_id=DUCKDB_CONN_ID)
